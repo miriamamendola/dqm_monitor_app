52da054d3382e8fad92f51272d118f6be4916cc6
miriamamendola <miriam.amendola.5@gmail.com>
2019-04-25 19:51:01 +0200

    changes
    -made Monitor screen Stateful
    -added test_notification.dart to test notifications
    -scheduling notifications still not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
-void main() {
+void main() async {
+  flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
   runApp(DqmMonitorApp());
 }
 
@@ -43,6 +49,7 @@ class AppDrawer extends StatelessWidget{
             ]
         )
     );}}
+
 class DqmMonitorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -58,7 +65,76 @@ class DqmMonitorApp extends StatelessWidget {
   }
 }
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget{
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+
+  var platform = MethodChannel('crossingthestreams.io/resourceResolver');
+
+  @override
+  initState() {
+    super.initState();
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidRecieveLocalNotification);
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+  Future<void> onDidRecieveLocalNotification(
+      int id, String title, String body, String payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(body),
+        actions: [
+          CupertinoDialogAction(
+              isDefaultAction: true,
+              child: Text('Ok')
+          )
+        ],
+      ),
+    );
+  }
+  Future<void> _showNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'plain title', 'plain body', platformChannelSpecifics,
+        payload: 'item x');
+  }
+    Future<void> _showDailyAtTime(Time scheduledTime) async {
+      var time = scheduledTime;
+      var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+          'repeatDailyAtTime channel id',
+          'repeatDailyAtTime channel name',
+          'repeatDailyAtTime description');
+      var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+      var platformChannelSpecifics = NotificationDetails(
+          androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.showDailyAtTime(
+          0,
+          'ELOG REMINDER',
+          'Daily notification shown at approximately ${(time.hour)}:${(time.minute)}:${(time.second)}',
+          time,
+          platformChannelSpecifics);
+    }
+
+  Future onSelectNotification(String payload) async {
+    return null;
+  }
   @override
   Widget build(BuildContext context) {return Scaffold(
       appBar: new AppBar(
@@ -67,19 +143,28 @@ class Settings extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           ListTile(
+            title: Text("Select your school"),
+            onTap: () {
+              //TODO:let a picker assign school_name on a var
+            }
+          ),
+          ListTile(
               title: Text("ELOG reminder"),
               subtitle: Text("Set an alarm which reminds you to check the MRPC"),
               onTap:() async {
                 TimeOfDay picked = await showTimePicker(context: context, initialTime: TimeOfDay.now());
+                //LOG STUFF
                 print(picked.hour);
                 print(picked.minute);
-                //TODO:add scheduled notification
+                Time scheduledTime = Time(picked.hour, picked.minute, 0);
+                _showDailyAtTime(scheduledTime);
               }),
           ListTile(
             title: Text("ELOG Alarm"),
             subtitle: Text("ELOG not taken alarm"),
-            onTap: () {
-              //TODO:add scheduled notification
+            onTap: () async {
+              //TODO: to implement but needs monitor activity working
+              await _showNotification();
             },
           )
         ],
@@ -111,7 +196,16 @@ class Docs extends StatelessWidget{
   }
 }
 
-class Monitor extends StatelessWidget {
+class Monitor extends StatefulWidget{
+  _MonitorState createState() => _MonitorState();
+}
+class _MonitorState extends State<Monitor> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
 
