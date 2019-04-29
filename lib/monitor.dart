acfdbcccc193d4359fde94a8779efb91b66df91e
miriamamendola <miriam.amendola.5@gmail.com>
2019-04-29 22:55:33 +0200

    changed parsing method now using html parsing dart libraries created this branch to develop notifications functions

--- lib/monitor.dart
+++ lib/monitor.dart
@@ -1,42 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'dart:convert';
 
 import 'main.dart';
 import 'drawer.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:html/parser.dart' show parse;
+
 //TODO: setting up something to update the json parsed file
-var url = "https://pastebin.com/raw/nPbjH8hM";
+const url = 'https://iatw.cnaf.infn.it/eee/monitor/';
+var document;
+
 
 //TODO: maybe since it's useful for other classes, I'll move this class in another file
-class JSONValues {
-  final int id;
-  final String telescopeStatus;
+class MonitorData {
   final String schoolName;
   final String filesSent;
   final String lastFileSent;
   final String lastElogEntry;
   final String rateOfTriggers;
   final String rateOfTracks;
-  final String dqmLink;
+  final String telescopeStatus;
 
-  JSONValues(
-      {this.id, this.telescopeStatus, this.schoolName, this.filesSent, this.lastFileSent, this.lastElogEntry, this.rateOfTriggers, this.rateOfTracks, this.dqmLink});
 
-  factory JSONValues.fromJson(Map<String, dynamic> json) {
-    return JSONValues(
-        id: json['id'],
-        telescopeStatus: json['telescope_status'][0],
-        schoolName: json['school_name'],
-        filesSent: json['files_sent'],
-        lastFileSent: json['last_file_sent'],
-        lastElogEntry: json['last_elog_entry'],
-        rateOfTriggers: json['rate_of_triggers'],
-        rateOfTracks: json['rate_of_tracks'],
-        dqmLink: json['dqm_link']
+  MonitorData(
+      {this.schoolName, this.filesSent, this.lastFileSent, this.lastElogEntry, this.rateOfTriggers, this.rateOfTracks, this.telescopeStatus});
+
+//qui metto il parser
+  factory MonitorData.parseHTML(String str) {
+    print(url);
+    final document = parse(str);
+    final table = document.body
+        .getElementsByTagName("table")
+        .first;
+    final tableRow = table.getElementsByTagName("tr")[selectedStation +
+        1]; //element
+
+    return MonitorData(
+        schoolName: tableRow.children[0].children[0].text,
+        //element
+        filesSent: tableRow.children[4].text.split("[")[0],
+        lastFileSent: tableRow.children[3].text,
+        lastElogEntry: tableRow.children[5].text,
+        rateOfTriggers: tableRow.children[8].text,
+        rateOfTracks: tableRow.children[9].text
     );
   }
+
 }
 
 class Monitor extends StatefulWidget{
@@ -49,7 +59,7 @@ class _MonitorState extends State<Monitor> {
   //final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
   //new GlobalKey<RefreshIndicatorState>();
 
-  JSONValues values;
+  MonitorData values;
 
   @override
   void initState() {
@@ -61,60 +71,70 @@ class _MonitorState extends State<Monitor> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: new AppBar(
-          title: new Text("Monitor DQM"),
+    try {
+      return Scaffold(
+          appBar: new AppBar(
+            title: new Text("Monitor DQM"),
 
-        ),
-        body: RefreshIndicator(onRefresh: _refresh,
-            child: ListView.separated(
-
-              itemCount: headings.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                    leading: headings[index],
-                    title: Text(monitorValues[index], textAlign: TextAlign.end)
-                );
-              },
-              separatorBuilder: (context, index) {
-                return Divider();
-              },
-            )),
-        drawer: AppDrawer()
-    );
+          ),
+          body: RefreshIndicator(onRefresh: _refresh,
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: headings[0],
+                    title: Text(values.schoolName, textAlign: TextAlign.end),
+                  ), Divider(),
+                  ListTile(
+                    leading: headings[1],
+                    title: Text(values.filesSent, textAlign: TextAlign.end),
+                  ), Divider(),
+                  ListTile(
+                    leading: headings[2],
+                    title: Text(values.lastFileSent, textAlign: TextAlign.end),
+                  ), Divider(),
+                  ListTile(
+                    leading: headings[3],
+                    title: Text(values.lastElogEntry, textAlign: TextAlign.end),
+                  ), Divider(),
+                  ListTile(
+                    leading: headings[4],
+                    title: Text(values.rateOfTracks, textAlign: TextAlign.end),
+                  ), Divider(),
+                  ListTile(
+                    leading: headings[5],
+                    title: Text(
+                        values.rateOfTriggers, textAlign: TextAlign.end),
+                  ), Divider(),
+                ],
+              )
+          ),
+          drawer: AppDrawer()
+      );
+    } catch (e) {
+      return CircularProgressIndicator();
+    }
   }
 
   Future<Null> _refresh() {
-    return fetchJSONValues().then((_values) {
+    return fetchMonitorData().then((_values) {
       setState(() {
         values = _values;
-        _createmonitorvalues(values);
       });
     });
   }
 
-  Future<JSONValues> fetchJSONValues() async {
+  Future<MonitorData> fetchMonitorData() async
+  {
     final response =
     await http.get(url);
 
     if (response.statusCode == 200) {
       // If server returns an OK response, parse the JSON
-      var jsonFile = json.decode(response.body);
-      return JSONValues.fromJson(jsonFile[selectedStation]);
+      return MonitorData.parseHTML(response.body);
     } else {
       // If that response was not OK, throw an error.
       throw Exception('Failed to load post');
     }
-  }
-
-  _createmonitorvalues(JSONValues jsonValues) {
-    monitorValues = [];
-    monitorValues.add(jsonValues.schoolName);
-    monitorValues.add(jsonValues.filesSent);
-    monitorValues.add(jsonValues.lastFileSent);
-    monitorValues.add(jsonValues.lastElogEntry);
-    monitorValues.add(jsonValues.rateOfTriggers);
-    monitorValues.add(jsonValues.rateOfTracks);
   }
 
 
